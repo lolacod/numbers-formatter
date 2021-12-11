03c61176f0a345d1684eb7888801c7b760f06a53
lolacod <lolacod@non.com>
2021-12-11 00:45:49 -0800

    Add bottom bar with github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar'
 
 
 function App() {
@@ -20,8 +21,24 @@ function App() {
         <Row>
           <Formatter/>
         </Row>
+        <Navbar bg="light">
+          <Navbar.Text>
+            © {new Date().getFullYear()} Genial Studio
+          </Navbar.Text>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <a href="https://github.com/lolacod/numbers-formatter">
+              <img
+                src="GitHub-Mark-32px.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="GitHub"
+              />
+            </a>
+          </Navbar.Collapse>          
+        </Navbar>        
     </Container>
-
   );
 }
 
